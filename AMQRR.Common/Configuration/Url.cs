73faf6cf3ea5847dd16f10947c9afbe42da082dd
dbl4k <github@dbl4k.net>
2019-04-27 10:23:00 +0100

    add url constants file

---
+++ AMQRR.Common/Configuration/Url.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMQRR.Common.Configuration
+{
+    public class Url
+    {
+        public const string API_URL = "http://localhost:65251/";
+        public const string BROKER_URL = "http://localhost:61616/";
+    }
+}
