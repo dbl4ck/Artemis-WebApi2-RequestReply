c5a8b4558e37871d0c54ed089b2f235e413de005
dbl4k <github@dbl4k.net>
2019-04-27 13:58:48 +0100

    add autoconnect feature to MqSession

--- AMQRR.Common/MQ/MqSession.cs
+++ AMQRR.Common/MQ/MqSession.cs
@@ -4,26 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+using Apache.NMS.Util;
 
 namespace AMQRR.Common.MQ
 {
     public class MqSession
     {
+        private string _brokerUri;
+        private bool _autoStart;
         private IConnectionFactory _connectionFactory;
         private IConnection _connection;
         private ISession _session;
 
-        public MqSession(string brokerUri)
+        public MqSession(string brokerUri, bool autoStart = true)
         {
-            BrokerUri = brokerUri;
+            _brokerUri = brokerUri;
+            _autoStart = autoStart;
         }
 
-        public string BrokerUri { get; }
+        public string BrokerUri => _brokerUri;
 
-        public IConnectionFactory ConnectionFactory => _connectionFactory ?? (_connectionFactory = new NMSConnectionFactory(BrokerUri));
+        public IConnectionFactory ConnectionFactory => _connectionFactory ?? (_connectionFactory = new ConnectionFactory(BrokerUri));
 
-        public IConnection Connection => _connection ?? (_connection = ConnectionFactory.CreateConnection());
+        //public IConnection Connection => _connection ?? (_connection = ConnectionFactory.CreateConnection());
 
+        public IConnection Connection
+        {
+            get
+            {
+                if (_connection == null) _connection = ConnectionFactory.CreateConnection(); ;
+
+                if (_autoStart && !_connection.IsStarted) _connection.Start();
+                
+                return _connection;
+            }
+        }
+        
         public ISession Session => _session ?? (_session = Connection.CreateSession());
     }
 }
