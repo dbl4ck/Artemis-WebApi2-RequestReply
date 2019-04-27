ae33c6b0269c22ba331768129d19c8d9eb4f4b44
dbl4k <github@dbl4k.net>
2019-04-27 11:37:44 +0100

    add session + connection properties to interface

---
+++ AMQRR.API/Services/Singleton/IMqService.cs
@@ -0,0 +1,12 @@
+﻿using AMQRR.Common.MQ;
+using Apache.NMS;
+
+namespace AMQRR.API.Services.Singleton
+{
+    public interface IMqService
+    {
+        MqSession MqSession { get; }
+        ISession Session { get; }
+        IConnection Connection { get; }
+    }
+}
