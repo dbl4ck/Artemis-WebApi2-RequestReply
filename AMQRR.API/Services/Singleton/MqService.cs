e4d91ebd65f1dbd7505255cc3ca1358c1a703ee9
dbl4k <github@dbl4k.net>
2019-04-27 11:08:57 +0100

    add mqService singleston to api

---
+++ AMQRR.API/Services/Singleton/MqService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using AMQRR.Common.MQ;
+using AMQRR.Common.Configuration;
+
+namespace AMQRR.API.Services.Singleton
+{
+    public class MqService
+    {
+        // static 
+        private static MqService _mqService;
+
+        private static MqService GetInstance() => _mqService ?? (_mqService = new MqService());
+
+        // instance
+        private MqSession _mqSession;
+
+        public MqSession MqSession() => _mqSession ?? (_mqSession = new MqSession(Url.BROKER_URL));
+
+    }
+}
