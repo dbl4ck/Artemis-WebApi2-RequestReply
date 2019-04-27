49a4f946c18929902f69e2011d802260a4960f38
dbl4k <github@dbl4k.net>
2019-04-27 14:27:37 +0100

    tidy spacing

--- AMQRR.Common/Models/Order.cs
+++ AMQRR.Common/Models/Order.cs
@@ -9,7 +9,7 @@ namespace AMQRR.Common.Models
     public class Order
     {
         public int OrderId { get; set; }
-        public int Customer{ get; set; }
+        public int Customer { get; set; }
         public List<OrderLine> OrderLines { get; set; }
     }
 }
