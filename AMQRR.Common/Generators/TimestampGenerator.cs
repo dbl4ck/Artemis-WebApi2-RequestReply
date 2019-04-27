f315b3ee9ab2dba6b8dafde5d4926e40db8cc1d3
dbl4k <github@dbl4k.net>
2019-04-27 17:44:18 +0100

    add timestamp generator

---
+++ AMQRR.Common/Generators/TimestampGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMQRR.Common.Generators
+{
+    public class TimestampGenerator
+    {
+        private string _format;
+        private string _prefix;
+        private string _suffix;
+
+        public TimestampGenerator(string format = "HH:mm:ss.ffffff", string prefix = "[", string suffix = "]")
+        {
+            _format = format;
+            _prefix = prefix;
+            _suffix = suffix;
+        }
+
+        public string Generate()
+        {
+            return $"{_prefix}{DateTime.Now.ToString(_format)}{_suffix}";
+        }
+
+        public string Format
+        {
+            get => _format;
+            set => _format = value;
+        } 
+
+    }
+} 
