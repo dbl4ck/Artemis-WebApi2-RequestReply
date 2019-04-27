391d482ad9f19453f4b1008f5681e3867e7513ec
dbl4k <github@dbl4k.net>
2019-04-27 12:20:47 +0100

    add queue constant class

---
+++ AMQRR.Common/Configuration/Queue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMQRR.Common.Configuration
+{
+    public class Queue
+    {
+        public const string API_ORDERS_POST = "api.orders.post";
+    }
+}
