ee4e37cba53e00e9beb9dc326d31ae0b2bc77fda
dbl4k <github@dbl4k.net>
2019-04-27 11:11:10 +0100

    add init mqservice call to app_start

--- AMQRR.API/Global.asax.cs
+++ AMQRR.API/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using AMQRR.Common.MQ;
 
 namespace AMQRR.API
 {
@@ -18,6 +19,9 @@ namespace AMQRR.API
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // initialize services
+            Services.Singleton.MqService.GetInstance();
         }
     }
 }
