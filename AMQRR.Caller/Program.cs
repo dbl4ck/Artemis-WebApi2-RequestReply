d6be10af7b0aeee02f7dfca11fe43f0aa15c9960
dbl4k <github@dbl4k.net>
2019-04-27 10:22:19 +0100

    add caller project

--- AMQRR.Caller/Program.cs
+++ AMQRR.Caller/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using AMQRR.Common.Factories;
+using AMQRR.Common.Configuration;
 
 namespace AMQRR.Caller
 {
     class Program
     {
+        public static string _baseUri = Url.API_URL;
+
         static void Main(string[] args)
         {
+            var orderFactory = new RandomOrderFactory();
+
+            using (var client = new HttpClient() { BaseAddress = new Uri(_baseUri) })
+            {
+                while (true)
+                {
+                    var order = orderFactory.Create();
+
+
+
+                }
+
+            }
+
         }
     }
 }
