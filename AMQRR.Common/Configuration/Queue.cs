0e16b64d88947b334b53b7c36216121e5d80213c
dbl4k <github@dbl4k.net>
2019-04-27 15:24:36 +0100

    add orders-get queue

--- AMQRR.Common/Configuration/Queue.cs
+++ AMQRR.Common/Configuration/Queue.cs
@@ -10,5 +10,8 @@ namespace AMQRR.Common.Configuration
     {
         public const string API_ORDERS_POST = "api.orders.post";
         public const string API_ORDERS_POST_REPLY = API_ORDERS_POST + ".reply";
+
+        public const string API_ORDERS_GET = "api.orders.get";
+        public const string API_ORDERS_GET_REPLY = API_ORDERS_GET + ".reply";
     }
 }
