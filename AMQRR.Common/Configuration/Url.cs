be26e73484b4fde64305eba4aa00df87b9fe14dc
dbl4k <github@dbl4k.net>
2019-04-27 10:48:55 +0100

    remove suffixed slash char

--- AMQRR.Common/Configuration/Url.cs
+++ AMQRR.Common/Configuration/Url.cs
@@ -8,7 +8,7 @@ namespace AMQRR.Common.Configuration
 {
     public class Url
     {
-        public const string API_URL = "http://localhost:65251/";
-        public const string BROKER_URL = "http://localhost:61616/";
+        public const string API_URL = "http://localhost:65251";
+        public const string BROKER_URL = "http://localhost:61616";
     }
 }
