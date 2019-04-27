a8bc589b3311f3a5092e786b0090bb2080bb0e9d
dbl4k <github@dbl4k.net>
2019-04-27 11:09:10 +0100

    add mqsession to common

---
+++ AMQRR.Common/MQ/MqSession.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apache.NMS;
+
+namespace AMQRR.Common.MQ
+{
+    public class MqSession
+    {
+        private IConnectionFactory _connectionFactory;
+        private IConnection _connection;
+        private ISession _session;
+
+        public MqSession(string brokerUri)
+        {
+            BrokerUri = brokerUri;
+        }
+
+        public string BrokerUri { get; }
+
+        public IConnectionFactory ConnectionFactory => _connectionFactory ?? (_connectionFactory = new NMSConnectionFactory(BrokerUri));
+
+        public IConnection Connection => _connection ?? (_connection = ConnectionFactory.CreateConnection());
+
+        public ISession Session => _session ?? (_session = Connection.CreateSession());
+    }
+}
