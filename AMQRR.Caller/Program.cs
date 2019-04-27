89195990d718dbc441e1b7596631262314787e40
dbl4k <github@dbl4k.net>
2019-04-27 10:48:39 +0100

    wait for post async and throw on response non-success

--- AMQRR.Caller/Program.cs
+++ AMQRR.Caller/Program.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Mime;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using AMQRR.Common.Factories;
 using AMQRR.Common.Configuration;
+using Newtonsoft.Json;
+using System.Threading;
+using System.Web;
 
 namespace AMQRR.Caller
 {
@@ -17,17 +21,25 @@ namespace AMQRR.Caller
         static void Main(string[] args)
         {
             var orderFactory = new RandomOrderFactory();
+            var httpClient = new HttpClient() {BaseAddress = new Uri(_baseUri)};
 
-            using (var client = new HttpClient() { BaseAddress = new Uri(_baseUri) })
+            while (true)
             {
-                while (true)
+                var order = orderFactory.Create();
+                var serialised = JsonConvert.SerializeObject(order,Formatting.Indented);
+                var stringContent = new StringContent(serialised, Encoding.UTF8,"application/json");
+
+                Console.WriteLine(serialised);
+
+                var post = httpClient.PostAsync("/api/orders", stringContent);
+                post.Wait();
+
+                if (!post.Result.IsSuccessStatusCode)
                 {
-                    var order = orderFactory.Create();
-
-
-
+                    throw new HttpException($"{post.Result.StatusCode} - {post.Result.ReasonPhrase}");
                 }
 
+                Thread.Sleep(1000);
             }
 
         }
