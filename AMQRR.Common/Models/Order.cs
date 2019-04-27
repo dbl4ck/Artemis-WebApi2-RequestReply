9ed1c9aae44cc54105b169ae85af4a343b94d640
dbl4k <github@dbl4k.net>
2019-04-27 10:21:26 +0100

    add order & orderline models

---
+++ AMQRR.Common/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMQRR.Common.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public int Customer{ get; set; }
+        public List<OrderLine> OrderLines { get; set; }
+    }
+}
---
+++ AMQRR.Common/Models/OrderLine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMQRR.Common.Models
+{
+    public class OrderLine
+    {
+        public int OrderLineId { get; set; }
+        public int OrderId { get; set; }
+        public int Sequence { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
