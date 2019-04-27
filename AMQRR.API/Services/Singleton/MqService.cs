75f2b99af9ccf0edb2fcd5248655cb86ca45b45f
dbl4k <github@dbl4k.net>
2019-04-27 11:38:04 +0100

    add connection and session properties to instance methods

--- AMQRR.API/Services/Singleton/MqService.cs
+++ AMQRR.API/Services/Singleton/MqService.cs
@@ -5,20 +5,20 @@ using System.Linq;
 using System.Web;
 using AMQRR.Common.MQ;
 using AMQRR.Common.Configuration;
+using Apache.NMS;
 
 namespace AMQRR.API.Services.Singleton
 {
-    public class MqService
+    public class MqService : IMqService
     {
         // static 
         private static MqService _mqService;
-
         public static MqService GetInstance() => _mqService ?? (_mqService = new MqService());
 
         // instance
         private MqSession _mqSession;
-
-        public MqSession MqSession() => _mqSession ?? (_mqSession = new MqSession(Url.BROKER_URL));
-
+        public MqSession MqSession => _mqSession ?? (_mqSession = new MqSession(Url.BROKER_URL));
+        public ISession Session => MqSession.Session;
+        public IConnection Connection => MqSession.Connection;
     }
 }
