080f3187b34de2200effcadae8ca343945fa8b90
dbl4k <github@dbl4k.net>
2019-04-28 10:58:52 +0100

    add nms filter generator to strongly type nms consumer filters

---
+++ AMQRR.Common/Generators/NMSFilterGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AMQRR.Common.Enumerations;
+
+namespace AMQRR.Common.Generators
+{
+    public class MqFilterGenerator
+    {
+        public const string AND = "AND";
+
+        private readonly Dictionary<NMSFilter, string> _filters;
+
+        public MqFilterGenerator()
+        {
+            _filters = new Dictionary<NMSFilter, string>();
+        }
+
+        public MqFilterGenerator Add(NMSFilter filter, string value)
+        {
+            _filters[filter] = value;
+
+            return this;
+        }
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+            var firstElement = true;
+
+            foreach (var filter in _filters)
+            {
+                if (firstElement != true)
+                {
+                    builder.Append($" {AND} ");
+                }
+                else
+                {
+                    firstElement = false;
+                }
+
+                builder.Append($"{filter.Key}='{filter.Value}'");
+            }
+
+            return builder.ToString();
+        }
+
+    }
+}
